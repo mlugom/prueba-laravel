3ca3997a8d1bdc4dd3ddd997c83941d3b1fa4152
Manuel <me.lm2207@gmail.com>
2024-09-22 09:39:14 -0500

    feat: create form view to create and edit registries

---
+++ resources/views/registros/form.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Crear nuevo registro</div>
+                    <div class="card-body">
+                        <form method="POST">
+                            @csrf
+                            <div>
+                                <label for="nombre" class="form-label">Nombre: </label>
+                                <input type="text" name="nombre" class="form-control" />
+                            </div>
+
+                            <div>
+                                <label for="descripcion" class="form-label">Descripción: </label>
+                                <textarea name="descripcion" id="descripcion" cols="30" rows="10" class="form-control"></textarea>
+                            </div>
+
+                            <div class="d-flex justify-content-center">
+                                <input type="submit" value="Guardar" class="btn btn-primary mt-3">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
