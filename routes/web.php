ddf708bac6fafae9835e12b158e472018404bc19
Manuel <me.lm2207@gmail.com>
2024-09-22 09:40:12 -0500

    feat: create registries resource routes and json

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RegistroController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +17,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/registros', RegistroController::class);
+    Route::get('/registros-json', [RegistroController::class, 'getAll'])->name('registros.json');
 });
 
 require __DIR__.'/auth.php';
